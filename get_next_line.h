ed22b42a37da2f28605cbb26f0cd92689e9460c9
Thomas BOUDER <tbouder@e3r9p16.42.fr>
2016-01-06 16:12:18 +0100

    GNL final with bonus

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 11:59:39 by tbouder           #+#    #+#             */
-/*   Updated: 2016/01/04 12:05:46 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/01/06 16:03:33 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <fcntl.h> //
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/libft.h"
