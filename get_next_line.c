472ba6bfe4b6efa3f529eaf76b702946a9051619
tbouder <tbouder@student.42.fr>
2016-08-23 00:14:42 +0200

    Correct a bug with big datas

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 11:59:39 by tbouder           #+#    #+#             */
-/*   Updated: 2016/07/28 19:01:54 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/08/23 00:12:35 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		ft_return_value(t_gnl gnl, char **line)
+static int		ft_return_value(t_gnl gnl, char **line)
 {
 	if (gnl.i == -1)
 		return (-1);
@@ -23,31 +23,42 @@ int		ft_return_value(t_gnl gnl, char **line)
 		return (1);
 }
 
-int		get_next_line(int const fd, char **line)
+static void		ft_buffer_manip(t_gnl *gnl)
+{
+	gnl->is_ln = ft_strlen(gnl->str);
+	gnl->l_len = ft_linelen(gnl->str, 0);
+	gnl->line = ft_strsub(gnl->str, 0, gnl->l_len);
+	gnl->tmp = ft_strsub(gnl->str, gnl->l_len + 1,
+		ft_strlen(&gnl->str[gnl->l_len]));
+	ft_strdel(&gnl->str);
+	gnl->str = ft_strinit(gnl->tmp);
+	ft_strdel(&gnl->tmp);
+}
+
+int				get_next_line(int const fd, char **line)
 {
 	static t_gnl	gnl;
 
 	if (line == NULL || fd == -1)
 		return (-1);
-	gnl.list = NULL;
-	gnl.buffer = ft_strnew(BUFF_SIZE);
-	!gnl.str ? gnl.str = ft_strnew(0) : 0;
-	while ((gnl.i = read(fd, gnl.buffer, BUFF_SIZE)) && gnl.buffer[0] != '\0')
+	if (!gnl.str)
 	{
-		gnl.tmp = ft_strinit(gnl.str);
-		ft_strdel(&gnl.str);
-		gnl.str = ft_strnjoin(gnl.tmp, gnl.buffer, ft_strlen(gnl.tmp), gnl.i);
+		gnl.buff = ft_strnew(BUFF_SIZE);
+		gnl.str = ft_strnew(0);
+		while ((gnl.i = read(fd, gnl.buff, BUFF_SIZE)) && gnl.buff[0] != '\0')
+		{
+			gnl.tmp = ft_strinit(gnl.str);
+			ft_strdel(&gnl.str);
+			gnl.str = ft_strnjoin(gnl.tmp, gnl.buff, ft_strlen(gnl.tmp), gnl.i);
+			ft_strdel(&gnl.tmp);
+			if (ft_isstrstr(gnl.buff, "\n"))
+				break ;
+		}
 		ft_strdel(&gnl.tmp);
-		if (ft_isstrstr(gnl.buffer, "\n"))
-			break ;
 	}
-	gnl.is_ln = ft_strlen(gnl.str);
-	gnl.l_len = ft_linelen(gnl.str, 0);
-	gnl.line = ft_strsub(gnl.str, 0, gnl.l_len);
-	gnl.tmp = ft_strsub(gnl.str, gnl.l_len + 1, ft_strlen(&gnl.str[gnl.l_len]));
-	ft_strdel(&gnl.str);
-	gnl.str = ft_strinit(gnl.tmp);
-	ft_strdel(&gnl.tmp);
+	ft_buffer_manip(&gnl);
 	*line = ft_strinit(gnl.line);
+	if (ft_strlen(gnl.str) == 0)
+		ft_strdel(&gnl.str);
 	return (ft_return_value(gnl, line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,13 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 11:59:39 by tbouder           #+#    #+#             */
-/*   Updated: 2016/07/28 20:20:43 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/08/23 00:14:03 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>
 # include "../libft.h"
 # define BUFF_SIZE 2000000
 
@@ -22,11 +20,10 @@ int		get_next_line(int const fd, char **line);
 
 typedef struct		s_gnl
 {
-	t_list			*list;
 	char			*str;
 	char			*line;
 	char			*tmp;
-	char			*buffer;
+	char			*buff;
 	int				i;
 	int				l_len;
 	int				is_ln;
